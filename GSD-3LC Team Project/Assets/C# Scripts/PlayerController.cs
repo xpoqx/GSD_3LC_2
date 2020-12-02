6c31db3ce27fa4178b3e87fa096b9cb03e83a522
june0556 <june0556@users.noreply.github.com>
2020-12-03 05:08:16 +0900

    Revert "201710920 노준"
    This reverts commit b363778c6d7ae618c74c97d963530779b5d14639.

--- GSD-3LC Team Project/Assets/C# Scripts/PlayerController.cs
+++ GSD-3LC Team Project/Assets/C# Scripts/PlayerController.cs
@@ -9,13 +9,9 @@ public class PlayerController : MonoBehaviour
     public int KeyCount;
     Animator animator;
     public int SideKey;
-    Rigidbody2D rigid;
-    Vector3 dirVec;
-    GameObject scanObject;
     // Start is called before the first frame update
     void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
         Player = GameObject.Find("Player");
         moveSpeed = 5f ;
         Debug.Log(Time.deltaTime);
@@ -26,8 +22,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
         if (KeyCount == 2)
         {
             moveSpeed = 3.8f;
@@ -48,28 +42,24 @@ public class PlayerController : MonoBehaviour
             Player.transform.Translate(0, moveSpeed * Time.deltaTime, 0);
             KeyCount++;
             SideKey = 0;
-            dirVec = Vector3.up;
         }
         if (Input.GetKey(KeyCode.S))
         {
             Player.transform.Translate(0, -moveSpeed * Time.deltaTime, 0);
             KeyCount++;
             SideKey = 0;
-            dirVec = Vector3.down;
         }
         if (Input.GetKey(KeyCode.A))
         {
             Player.transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
             KeyCount++;
             SideKey = -1;
-            dirVec = Vector3.left;
         }
         if (Input.GetKey(KeyCode.D))
         {
             Player.transform.Translate(moveSpeed * Time.deltaTime, 0,0);
             KeyCount++;
             SideKey = 1;
-            dirVec = Vector3.right;
         }
         if (SideKey != 0)
         {
@@ -80,26 +70,7 @@ public class PlayerController : MonoBehaviour
         {
             animator.Play("DownWalk");
         }
+        
 
-        if(Input.GetButtonDown("Jump") && scanObject != null)
-        {
-            Debug.Log("This is : " + scanObject.name); // 오브젝트 앞에서 스페이스바를 누르면 그 오브젝트의 이름 출력
-        }
-
-    }
-
-    void FixedUpdate()
-    {
-        Debug.DrawRay(rigid.position, dirVec * 1.0f, Color.red);
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object")); //물체는 Layers를 Object로 바꿔서 구분한다
-
-        if (rayHit.collider != null)
-        {
-            scanObject = rayHit.collider.gameObject;
-        }
-        else
-        {
-            scanObject = null;
-        }
     }
 }
