694b24fed36438b06c8d3b642a6e19b25606f7d8
201811145 김주헌 <70737560+xpoqx@users.noreply.github.com>
2020-12-02 13:48:23 +0900

    12/02 13:38

---
+++ GSD-3LC Team Project/Assets/C# Scripts/SceneDirector.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SceneDirector : MonoBehaviour
+{
+    GameObject Scene1, Scene2;
+    public Text text1;
+    public static int SceneNo;
+    public int ChangeScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneNo = 1;
+        Scene1 = GameObject.Find("Scene1");
+        Scene2 = GameObject.Find("Scene2");
+        text1 = GameObject.Find("Text").GetComponent<Text>();
+        
+        ChangeScene = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && SceneNo <= 3)
+        {
+            SceneNo++;
+            Debug.Log(SceneNo);
+            ChangeScene = 1;
+            if (SceneNo == 4)
+            {
+                SceneManager.LoadScene("Tutorial Scene");
+            }
+        }
+        else if (Input.GetMouseButtonDown(1) && SceneNo >= 2)
+        {
+            SceneNo--;
+            Debug.Log(SceneNo);
+            ChangeScene = 1;
+        }
+        if (ChangeScene == 1)
+        {
+            {
+                switch (SceneNo)
+                {
+                    case 1:
+                        Scene1.SetActive(true);
+                        Scene2.SetActive(false);
+                        text1.text = "오늘도 평범하게 힘든 하루였다..";
+                        break;
+                    case 2:
+                        Scene2.SetActive(true);
+                        Scene1.SetActive(false);
+                        text1.text = "뭐야";
+                        break;
+                    case 3:
+                        Destroy(Scene1);
+                        Destroy(Scene2);
+                        text1.text = "3번째 텍스트";
+                        break;
+                }
+                ChangeScene = 0;
+            }
+        }
+    }
+}
---
+++ GSD-3LC Team Project/Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    GameObject Player;
+    public float moveSpeed;
+    public int KeyCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        moveSpeed = 5f ;
+        Debug.Log(Time.deltaTime);
+        KeyCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (KeyCount >= 2)
+        {
+            moveSpeed = 3f;
+            KeyCount = 0;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Player.transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+            KeyCount++;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Player.transform.Translate(0, -moveSpeed * Time.deltaTime, 0);
+            KeyCount++;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Player.transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+            KeyCount++;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Player.transform.Translate(moveSpeed * Time.deltaTime, 0,0);
+            KeyCount++;
+        }
+        moveSpeed = 5f;
+        
+
+    }
+}
