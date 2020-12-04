2b14c70753956a2ac3764d3cc0d4970ed7ac8ffe
201811145 김주헌 <70737560+xpoqx@users.noreply.github.com>
2020-12-04 23:33:28 +0900

    1204 2333
    컷씬 추가

--- GSD-3LC Team Project/Assets/C# Scripts/SceneDirector.cs
+++ GSD-3LC Team Project/Assets/C# Scripts/SceneDirector.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public class SceneDirector : MonoBehaviour
 {
-    GameObject Scene1, Scene2, Scene3, Scene4;
+    GameObject Scene1, Scene2, Scene3, Scene4,Scene5;
     public Text text1;
     public static int SceneNo;
     public int ChangeScene;
@@ -24,12 +24,14 @@ public class SceneDirector : MonoBehaviour
         Scene2 = GameObject.Find("CutScene2");
         Scene3 = GameObject.Find("CutScene3");
         Scene4 = GameObject.Find("CutScene4");
+        Scene5 = GameObject.Find("CutScene5");
         text1 = GameObject.Find("Text").GetComponent<Text>();
         //talkpanel.SetActive(true);
         ChangeScene = 1;
         Scene2.SetActive(false);
         Scene3.SetActive(false);
         Scene4.SetActive(false);
+        Scene5.SetActive(false);
         Scene1.SetActive(true);
     }
 
@@ -41,7 +43,7 @@ public class SceneDirector : MonoBehaviour
             
             Debug.Log(SceneNo);
             ChangeScene = 1;
-            if (SceneNo == 4)
+            if (SceneNo == 5)
             {
                 SceneManager.LoadScene("Tutorial Scene");
             }
@@ -61,13 +63,13 @@ public class SceneDirector : MonoBehaviour
                     case 1:
                         Scene1.SetActive(true);
                         Scene2.SetActive(false);
-                        text1.text = "하.. 코딩";
+                        text1.text = "하... 코딩...";
                         break;
                     case 2:
                         Scene2.SetActive(true);
                         Scene1.SetActive(false);
                         Scene3.SetActive(false);
-                        text1.text = "이 게임은 코딩을 어떻게 했길레 버그가 이렇게 많아..?";
+                        text1.text = "이 게임은 코딩을 어떻게 했길레 버그가 이렇게 많아??";
                         break;
                     case 3:
                         Scene3.SetActive(true);
@@ -78,9 +80,13 @@ public class SceneDirector : MonoBehaviour
                     case 4:
                         Scene3.SetActive(false);
                         Scene4.SetActive(true);
-                        text1.text = "으음...응?";
+                        Scene5.SetActive(false);
+                        text1.text = "드르렁...";
                         break;
                     case 5:
+                        Scene4.SetActive(false);
+                        Scene5.SetActive(true);
+                        text1.text = "으음... ...응?";
                         break;
                 }
                 ChangeScene = 0;
